74f3479373304b81ed92a69125074d317b1da7f7
Sylvio3365 <sylvioandri@gmail.com>
2025-07-01 10:42:49 +0300

    ie

--- src/main/java/com/biblio/bibliotheque/controller/LoginController.java
+++ src/main/java/com/biblio/bibliotheque/controller/LoginController.java
@@ -1,11 +1,12 @@
 package com.biblio.bibliotheque.controller;
 
-import com.biblio.bibliotheque.service.UtilisateurService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import com.biblio.bibliotheque.service.gestion.UtilisateurService;
 
 @Controller
 public class LoginController {
