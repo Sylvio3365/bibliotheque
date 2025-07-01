be5ef89af09f0d6ea1f29dc86c7c4c8ad0b2a2db
Sylvio3365 <sylvioandri@gmail.com>
2025-07-01 10:03:56 +0300

    correction

--- src/main/java/com/biblio/bibliotheque/repository/gestion/JourFerieRepository.java
+++ src/main/java/com/biblio/bibliotheque/repository/gestion/JourFerieRepository.java
@@ -4,9 +4,6 @@ import com.biblio.bibliotheque.model.gestion.JourFerie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.Optional;
-
 @Repository
 public interface JourFerieRepository extends JpaRepository<JourFerie, Integer> {
     /*
