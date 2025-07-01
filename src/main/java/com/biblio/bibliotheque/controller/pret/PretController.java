8c0ee0b14cbc192bf7f7d490da737dd6f07e159d
icecoldfinishing <rohyamboara@gmail.com>
2025-07-01 13:42:53 +0300

    swwq

--- src/main/java/com/biblio/bibliotheque/controller/pret/PretController.java
+++ src/main/java/com/biblio/bibliotheque/controller/pret/PretController.java
@@ -1,12 +1,10 @@
 package com.biblio.bibliotheque.controller.pret;
 
 import com.biblio.bibliotheque.model.pret.Pret;
-import com.biblio.bibliotheque.model.livre.Exemplaire;
-import com.biblio.bibliotheque.model.gestion.Adherent;
-import com.biblio.bibliotheque.model.livre.Type;
-import com.biblio.bibliotheque.repository.pret.*;
-import com.biblio.bibliotheque.repository.gestion.*;
-import com.biblio.bibliotheque.repository.livre.*;
+import com.biblio.bibliotheque.repository.pret.PretRepository;
+import com.biblio.bibliotheque.repository.gestion.AdherentRepository;
+import com.biblio.bibliotheque.repository.livre.ExemplaireRepository;
+import com.biblio.bibliotheque.repository.livre.TypeRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/pret")
+@RequestMapping("/preter")
 public class PretController {
 
     @Autowired
@@ -31,54 +29,14 @@ public class PretController {
     @Autowired
     private TypeRepository typeRepository;
 
-    // LIST
-    @GetMapping
-    public String listPrets(Model model) {
-        List<Pret> prets = pretRepository.findAll();
-        model.addAttribute("prets", prets);
-        return "views/pret/list";
-    }
 
-    // ADD FORM
-    @GetMapping("/add")
-    public String showAddForm(Model model) {
+
+    @GetMapping("/formpreter/livre")
+    public String showFormPreterLivre(Model model) {
         model.addAttribute("pret", new Pret());
         model.addAttribute("exemplaires", exemplaireRepository.findAll());
         model.addAttribute("adherents", adherentRepository.findAll());
         model.addAttribute("types", typeRepository.findAll());
-        return "views/pret/add";
-    }
-
-    // ADD ACTION
-    @PostMapping("/add")
-    public String addPret(@ModelAttribute Pret pret) {
-        pretRepository.save(pret);
-        return "redirect:/pret";
-    }
-
-    // EDIT FORM
-    @GetMapping("/edit/{id}")
-    public String showEditForm(@PathVariable("id") Integer id, Model model) {
-        Pret pret = pretRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid ID: " + id));
-        model.addAttribute("pret", pret);
-        model.addAttribute("exemplaires", exemplaireRepository.findAll());
-        model.addAttribute("adherents", adherentRepository.findAll());
-        model.addAttribute("types", typeRepository.findAll());
-        return "views/pret/edit";
-    }
-
-    // UPDATE ACTION
-    @PostMapping("/edit/{id}")
-    public String updatePret(@PathVariable("id") Integer id, @ModelAttribute Pret pret) {
-        pret.setId_pret(id);
-        pretRepository.save(pret);
-        return "redirect:/pret";
-    }
-
-    // DELETE ACTION
-    @GetMapping("/delete/{id}")
-    public String deletePret(@PathVariable("id") Integer id) {
-        pretRepository.deleteById(id);
-        return "redirect:/pret";
+        return "views/preter/form_preter";
     }
 }
