dd52fd11fd79de5daf04d0a2477875ccaed273eb
AHSanda2005 <andrianaivosonsanda@gmail.com>
2025-07-01 15:08:47 +0300

    ok

--- src/main/java/com/biblio/bibliotheque/controller/pret/PretController.java
+++ src/main/java/com/biblio/bibliotheque/controller/pret/PretController.java
@@ -2,6 +2,7 @@ package com.biblio.bibliotheque.controller.pret;
 
 import com.biblio.bibliotheque.model.pret.Pret;
 import com.biblio.bibliotheque.service.gestion.AdherentService;
+import com.biblio.bibliotheque.service.livre.ExemplaireService;
 import com.biblio.bibliotheque.repository.pret.PretRepository;
 import com.biblio.bibliotheque.repository.livre.ExemplaireRepository;
 import com.biblio.bibliotheque.repository.livre.TypeRepository;
@@ -29,6 +30,9 @@ public class PretController {
     @Autowired
     private AdherentService adherentService;
 
+    @Autowired
+    private ExemplaireService exemplaireService;
+
     @GetMapping("/formpreter/livre")
     public String showFormPreterLivre(Model model) {
         model.addAttribute("pret", new Pret());
@@ -41,21 +45,28 @@ public class PretController {
 public String savePret(@ModelAttribute Pret pret, Model model) {
     Integer idAdherent = pret.getAdherent().getIdAdherent();
     LocalDate dateDebut = pret.getDate_debut();
+    Integer idExemplaire = pret.getExemplaire().getId_exemplaire();
 
     String statut = adherentService.getStatutAdherentOnDate(idAdherent, dateDebut);
+    boolean disponible = exemplaireService.isExemplaireDisponible(idExemplaire);
 
     model.addAttribute("dateDebut", dateDebut);
     model.addAttribute("idAdherent", idAdherent);
     model.addAttribute("statut", statut);
+    model.addAttribute("idExemplaire", idExemplaire);
+    model.addAttribute("disponible", disponible);
 
-    if ("actif".equals(statut)) {
-        model.addAttribute("message", "Prêt reçu : adhérent actif.");
+    if (!disponible) {
+        model.addAttribute("message", "L'exemplaire n'est pas disponible.");
+    } else if ("actif".equalsIgnoreCase(statut)) {
+        model.addAttribute("message", "Prêt reçu : exemplaire disponible, adhérent actif.");
     } else {
-        model.addAttribute("message", "Prêt reçu : adhérent inactif.");
+        model.addAttribute("message", "Prêt reçu : exemplaire disponible, mais adhérent inactif.");
     }
 
     return "views/preter/verification_pret";
 }
+
 
 
 }
