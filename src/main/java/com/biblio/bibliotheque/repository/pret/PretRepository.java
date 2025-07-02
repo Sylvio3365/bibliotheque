0aef24591b6d197fb07b663e7d5100118d8c841c
AHSanda2005 <andrianaivosonsanda@gmail.com>
2025-07-02 09:55:46 +0300

    ok

--- src/main/java/com/biblio/bibliotheque/repository/pret/PretRepository.java
+++ src/main/java/com/biblio/bibliotheque/repository/pret/PretRepository.java
@@ -9,13 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PretRepository extends JpaRepository<Pret, Integer> {
-@Query("SELECT COUNT(p) FROM Pret p " +
-       "WHERE p.adherent.idAdherent = :idAdherent " +
-       "AND p.date_debut <= :nouvelleDate " +
-       "AND (p.date_fin IS NULL OR p.date_fin > :nouvelleDate) " +
-       "AND p.type.nom <> 'Sur place'")
-int countPretsActifsParAdherentALaDate(
-    @Param("idAdherent") Integer idAdherent, 
-    @Param("nouvelleDate") LocalDate nouvelleDate);
+
 
 }
